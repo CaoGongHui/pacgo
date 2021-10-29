410fca1a7ce77c00f4c2b18001c175398d15644c
CaoGongHui <847781669@qq.com>
2021-10-29 17:52:41 +0800

    添加load逻辑和print screen逻辑

--- main.go
+++ main.go
@@ -1,19 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var maze []string
 
 func main() {
 	// initiallize the game
 	// load resources
+	err := loadMaze("maze01.txt")
+	if err != nil {
+		log.Println("Failed to load maze: ", err)
+		return
+	}
 	// game loop
 	for {
 		// update screen
+		printScreen()
 		// process input
 		// process movement
 		// process collisions
 		// check game over
 		// temp : break infinite loop
-		fmt.Println("Hello, Pac go!")
 		break
+	}
+}
+func loadMaze(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+	return nil
+}
+
+func printScreen() {
+	for _, line := range maze {
+		fmt.Println(line)
 	}
 }
