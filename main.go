8f292a08197a6b3397132195bf18cbc42c0efd58
CaoGongHui <847781669@qq.com>
2021-10-29 17:38:58 +0800

    init pac go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// initiallize the game
+	// load resources
+	// game loop
+	for {
+		// update screen
+		// process input
+		// process movement
+		// process collisions
+		// check game over
+		// temp : break infinite loop
+		fmt.Println("Hello, Pac go!")
+		break
+	}
+}
