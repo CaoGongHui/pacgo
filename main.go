5d10c32925fcac290c10b640baca6fde6d8f759b
CaoGongHui <847781669@qq.com>
2021-11-05 14:20:19 +0800

    添加游戏循环功能和读取用户输入的功能

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+
+	"github.com/danicat/simpleansi"
 )
 
 var maze []string
 
 func main() {
 	// initiallize the game
+	initialize()
+	defer cleanup()
 	// load resources
 	err := loadMaze("maze01.txt")
 	if err != nil {
@@ -22,6 +27,14 @@ func main() {
 		// update screen
 		printScreen()
 		// process input
+		input, err := readInput()
+		if err != nil {
+			log.Println("Failed to read input: ", err)
+			break
+		}
+		if input == "ESC" {
+			break
+		}
 		// process movement
 		// process collisions
 		// check game over
@@ -44,7 +57,40 @@ func loadMaze(file string) error {
 }
 
 func printScreen() {
+	simpleansi.ClearScreen()
 	for _, line := range maze {
 		fmt.Println(line)
 	}
+}
+
+func initialize() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalln("Failed to set terminal to cbreak mode: ", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalln("Unable to restore cooked mode: ", err)
+	}
+}
+
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+	return "", nil
 }
